e95e3a38ee1573f6bcaf1309455e849cc503136b
Khagan95 <85906243+Khagan95@users.noreply.github.com>
2023-08-23 18:07:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace KarakterDegistirmeUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String ifade: ");
+            string ifade = Console.ReadLine();
+
+            KarakterDegistirici karakterDegistirici = new KarakterDegistirici();
+            string sonuc = karakterDegistirici.KarakterDegistir(ifade);
+
+            Console.WriteLine("Sonuç: " + sonuc);
+        }
+    }
+
+    class KarakterDegistirici
+    {
+        public string KarakterDegistir(string ifade)
+        {
+            char[] karakterler = ifade.ToCharArray();
+
+            for (int i = 0; i < karakterler.Length - 1; i += 2)
+            {
+                char temp = karakterler[i];
+                karakterler[i] = karakterler[i + 1];
+                karakterler[i + 1] = temp;
+            }
+
+            return new string(karakterler);
+        }
+    }
+}
